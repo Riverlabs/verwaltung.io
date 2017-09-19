127880efb25d422e4e626100536ea13704118068
Jens Ravens <jens@nerdgeschoss.de>
2017-09-19 19:58:29 +0200

    setup react

---
+++ client/components/App.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+import { Forms } from '../../lib/collections';
+
+class App extends Component {
+  render() {
+    return (
+      <main>
+        <h1>Hallo Welt!</h1>
+        {
+          this.props.forms.map(form => {
+            return <div key={form._id}>{form.name}</div>
+          })
+        }
+      </main>
+    );
+  }
+}
+
+export default createContainer(() => {
+  return {
+    forms: Forms.find({}).fetch(),
+  };
+}, App);
