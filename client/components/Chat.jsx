1cacfc5647ff2ebdaca817eea9cd5d8472f7c13e
Jens Ravens <jens@nerdgeschoss.de>
2017-09-19 22:50:54 +0200

    react insertion tests

--- client/components/Chat.jsx
+++ client/components/Chat.jsx
@@ -1,24 +1,39 @@
 import React, { Component } from 'react';
-import { createContainer } from 'meteor/react-meteor-data';
-import { Forms } from '../../lib/collections';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Conversations } from '../../lib/collections';
+import { Random } from 'meteor/random';
 
 class Chat extends Component {
+  sendMessage(event){
+    console.log(event);
+    const messageText = event.target.text.value;
+    const message = { type: "text", sender: "user", text: messageText, _id: Random.id };
+    Conversations.update({_id: this.props.conversation._id}, { $push: { messages: message} });
+  }
+
   render() {
+    const conversation = this.props.conversation;
+    if(!conversation) {
+      return null;
+    }
+    const messages = conversation.messages || [];
     return (
       <main>
-        <h1>Hallo Welt!</h1>
         {
-          this.props.forms.map(form => {
-            return <div key={form._id}>{form.name}</div>
+          messages.map(message => {
+            return <div key={message._id}>{message.text}</div>
           })
         }
+        <form onSubmit={this.sendMessage.bind(this)}>
+          <input name="text"/>
+        </form>
       </main>
     );
   }
 }
 
-export default createContainer(() => {
+export default withTracker(() => {
   return {
-    forms: Forms.find({}).fetch(),
+    conversation: Conversations.findOne({}),
   };
-}, Chat);
+})(Chat);
--- client/main.js
+++ client/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
-import { Forms } from '../lib/collections';
+import { Forms, Conversations } from '../lib/collections';
 import { renderRoutes } from './routes';
 
 Meteor.startup(() => {
@@ -9,3 +9,4 @@ Meteor.startup(() => {
 });
 
 window.Forms = Forms
+window.Conversations = Conversations
